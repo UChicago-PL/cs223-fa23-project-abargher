19b791df5eb09871a2ef7c95a3a758fa2dad26d3
Alec Bargher <abargher@uchicago.edu>
2023-11-27 15:03:17 -0600

    added buildImage functionality and required packages

--- Stars.hs
+++ Stars.hs
@@ -1,7 +1,23 @@
 module Stars where
 
+import Data.List
+import           Graphics.Image (Image, Pixel(..), RGB, VU(VU))
+import           Graphics.Image.ColorSpace
+import           Graphics.Image.Interface (MArray)
+import qualified Graphics.Image as Image
+
 buildImage :: FilePath -> Int -> Int -> [(Int, Int)] -> IO ()
-buildImage = undefined
+buildImage path width height centers = 
+  let
+    black = Image.PixelY 0.0
+    white = Image.PixelY 1.0
+    locs = groupBy (\(a, _) (b, _) -> a == b)
+            [(i, j) | i <- [0..height-1], j <- [0..width-1]]
+    pixels = map (map (\pr -> if pr `elem` centers then white else black)) locs
+    img :: Image VU Y Double = Image.fromListsR VU pixels
+  in
+    Image.writeImage path img
+
 
 main :: IO ()
 main = print "hello"

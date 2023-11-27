3a2d1beb9d0d9ca4a4b16eaf58e9dac50a6d97d0
Alec Bargher <abargher@uchicago.edu>
2023-11-27 15:09:39 -0600

    alec: factored out pixel determination into subroutine for easy modification

--- Stars.hs
+++ Stars.hs
@@ -6,14 +6,24 @@ import           Graphics.Image.ColorSpace
 import           Graphics.Image.Interface (MArray)
 import qualified Graphics.Image as Image
 
-buildImage :: FilePath -> Int -> Int -> [(Int, Int)] -> IO ()
+type Point = (Int, Int)
+
+getPixels :: [[Point]] -> [Point] -> [[Pixel Y Double]]
+getPixels locs centers = 
+  let
+    black = Image.PixelY 0.0
+    white = Image.PixelY 1.0
+  in
+    map (map (\pr -> if pr `elem` centers then white else black)) locs
+
+buildImage :: FilePath -> Int -> Int -> [Point] -> IO ()
 buildImage path width height centers = 
   let
     black = Image.PixelY 0.0
     white = Image.PixelY 1.0
     locs = groupBy (\(a, _) (b, _) -> a == b)
             [(i, j) | i <- [0..height-1], j <- [0..width-1]]
-    pixels = map (map (\pr -> if pr `elem` centers then white else black)) locs
+    pixels = getPixels locs centers
     img :: Image VU Y Double = Image.fromListsR VU pixels
   in
     Image.writeImage path img

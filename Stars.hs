05196bbb5292e0c60c54f01c9c0d74ac09e56864
Alec Bargher <abargher@uchicago.edu>
2023-11-27 16:44:35 -0600

    alec: simplified randomness to improve runtime

--- Stars.hs
+++ Stars.hs
@@ -14,6 +14,15 @@ type Point = (Int, Int)
 
 -- type Rand a = State StdGen a
 
+-- Taken directly from source at 
+-- https://hackage.haskell.org/package/list-grouping-0.1.1/docs/Data-List-Grouping.html#v%3asplitEvery
+-- source code: https://hackage.haskell.org/package/list-grouping-0.1.1/docs/src/Data-List-Grouping.html#splitEvery
+-- Only needed this function, not worth installing a whole additional package.
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery _ [] = []
+splitEvery n xs = as : splitEvery n bs
+     where (as,bs) = splitAt n xs
+
 distance :: Point -> Point -> Double
 distance (i1,j1) (i2,j2) =
   let
@@ -30,37 +39,44 @@ identifyCenters (newPoint:as) density threshold centers = do
     g <- get
     let (randVal, newGen) = uniformR (0 :: Double, 1 :: Double) g
     put newGen
-    if all (\p -> distance p newPoint >= threshold) centers && (randVal <= density) then
+    -- if all (\p -> distance p newPoint >= threshold) centers && (randVal <= density) then
+    if randVal <= density then
       identifyCenters as density threshold (newPoint : centers)
     else
       identifyCenters as density threshold centers
 
-getPixels :: [[Point]] -> [Point] -> [[Pixel Y Double]]
-getPixels locs centers =
-  let
-    black = Image.PixelY 0.0
-    white = Image.PixelY 1.0
-  in
-    map (map (\pr -> if pr `elem` centers then white else black)) locs
+black = Image.PixelY 0.0
+white = Image.PixelY 1.0
+
+getPixels :: [Point] -> [Point] -> [Pixel Y Double]
+getPixels [] _ = []
+getPixels rest [] = replicate (length rest) black
+getPixels (p:ps) cs@(c:cs') =
+  if p == c then
+    white : getPixels ps cs'
+  else
+    black : getPixels ps cs
 
 buildImage :: FilePath -> Int -> Int -> [Point] -> IO ()
 buildImage path width height centers =
   let
     black = Image.PixelY 0.0
     white = Image.PixelY 1.0
-    locs = groupBy (\(a, _) (b, _) -> a == b)
-            [(i, j) | i <- [0..height-1], j <- [0..width-1]]
-    pixels = getPixels locs centers
+    -- locs = groupBy (\(a, _) (b, _) -> a == b)
+    --         [(i, j) | i <- [0..height-1], j <- [0..width-1]]
+    locs = [(i, j) | i <- [0..height-1], j <- [0..width-1]]
+    pixels = splitEvery width $ getPixels locs (sort centers)
     img :: Image VU Y Double = Image.fromListsR VU pixels
   in
     Image.writeImage path img
 
 
-height = 100
-width = 100
+height = 1000
+width = 1000
 
 main :: IO ()
 main = do
   stdGen <- initStdGen
-  let centers = evalState (identifyCenters (evalState (shuffle [(i, j) | i <- [0..height-1], j <- [0..width-1]]) stdGen) 0.1 8.0 []) stdGen
+  -- let centers = evalState (identifyCenters (evalState (shuffle [(i, j) | i <- [0..height-1], j <- [0..width-1]]) stdGen) 0.1 8.0 []) stdGen
+  let centers = evalState (identifyCenters [(i, j) | i <- [0..height-1], j <- [0..width-1]] 0.001 8.0 []) stdGen
   buildImage "test-1.png" width height centers

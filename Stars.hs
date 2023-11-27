4ff0bedf369ba0b8458cc3dce6a5bbcb2c4984e8
Victor Brown <victor.fbrown4@gmail.com>
2023-11-27 15:56:37 -0600

    creating actual stars

--- Stars.hs
+++ Stars.hs
@@ -9,26 +9,28 @@ import           Graphics.Image (Image, Pixel(..), RGB, VU(VU))
 import           Graphics.Image.ColorSpace
 import           Graphics.Image.Interface (MArray)
 import qualified Graphics.Image as Image
+import MonadicShuffle
 type Point = (Int, Int)
 
-type Rand a = State StdGen a
+-- type Rand a = State StdGen a
 
 distance :: Point -> Point -> Double
 distance (i1,j1) (i2,j2) =
     sqrt ((fromIntegral i1  - fromIntegral i2)^2 + (fromIntegral j1 - fromIntegral j2)^2)
 
-identifyCenters :: Point -> Double -> Double -> [Point] -> Rand [Point]
-identifyCenters newPoint density threshold centers = do
+identifyCenters :: [Point] -> Double -> Double -> [Point] -> Rand [Point]
+identifyCenters [] _ _ centers = pure centers
+identifyCenters (newPoint:as) density threshold centers = do
     g <- get
     let (randVal, newGen) = uniformR (0,1) g
     put newGen
-    if all (\p -> distance p newPoint <= threshold) centers && (randVal <= density) then
-        pure [newPoint]
+    if all (\p -> distance p newPoint >= threshold) centers && (randVal <= density) then
+      identifyCenters as density threshold (newPoint : centers)
     else
-        pure []
+      identifyCenters as density threshold centers
 
 getPixels :: [[Point]] -> [Point] -> [[Pixel Y Double]]
-getPixels locs centers = 
+getPixels locs centers =
   let
     black = Image.PixelY 0.0
     white = Image.PixelY 1.0
@@ -36,7 +38,7 @@ getPixels locs centers =
     map (map (\pr -> if pr `elem` centers then white else black)) locs
 
 buildImage :: FilePath -> Int -> Int -> [Point] -> IO ()
-buildImage path width height centers = 
+buildImage path width height centers =
   let
     black = Image.PixelY 0.0
     white = Image.PixelY 1.0
@@ -48,5 +50,11 @@ buildImage path width height centers =
     Image.writeImage path img
 
 
+height = 100
+width = 100
+
 main :: IO ()
-main = print "hello"
+main = do
+  stdGen <- initStdGen
+  let centers = evalState (identifyCenters (evalState (shuffle [(i, j) | i <- [0..height-1], j <- [0..width-1]]) stdGen) 0.1 8.0 []) stdGen
+  buildImage "test-1.png" width height centers
